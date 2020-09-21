3e0421b6d29689bdfae52d74594184f8f15168a9
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2020-09-20 22:24:00 -0300

    Correcion en la ruta de noticias

--- src/routes/noticia.routes.js
+++ src/routes/noticia.routes.js
@@ -9,8 +9,8 @@ const {getPrueba, crearNoticia, listarNoticia, eliminarNotica, actualizarNoticia
 /* router.route('/Noticia').get(listarNoticia).post(crearNoticia); */
 router.route('/').get(listarNoticia)
 /* router.route('/Administracion/Editor/').post(crearNoticia); */
-router.route('Administracion/Noticia').get(listarNoticia).post(crearNoticia);
-router.route('Administracion/Noticia/:id').delete(eliminarNotica).put(actualizarNoticia);
+router.route('/Administracion/Noticia').get(listarNoticia).post(crearNoticia);
+router.route('/Administracion/Noticia/:id').delete(eliminarNotica).put(actualizarNoticia);
 /* router.route('/Administracion/Noticia/:id').delete(eliminarNotica); */
 
 export default router;
