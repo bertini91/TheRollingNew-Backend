d79203bf3f37a1e477c950e2b957aae44dc9253b
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-09-29 18:13:11 -0300

    no se que cambios habia aqui

--- src/index.js
+++ src/index.js
@@ -11,12 +11,23 @@ import './database'
 
 const app = express();
 
+
 app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), () => {
     console.log(path.join(__dirname, "../public"))
   console.log("Estoy escuchando puertoooo = " + app.get("port"));
+
 });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, x-access-token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 
 app.use(morgan('dev'))
 app.use(cors());
